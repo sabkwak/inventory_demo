e33ed6535fe374027e8d520e53cc057470c22959
kwakaflocka <sabkwak@gmail.com>
2024-11-21 21:45:11 -0700

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,4 @@
-export type TransactionType = "order" | "returns";
+export type TransactionType = "subtract" | "add";
 export type ProductType = "import";
 export type Timeframe = "month" | "year";
 export type Period = { year: number; month: number };
