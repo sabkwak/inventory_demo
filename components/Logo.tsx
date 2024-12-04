d0c4b8d60080def4333ad1031a296dfa7d613c6c
sabkwak <sabkwak@gmail.com>
2024-12-03 17:11:04 -0700

    replace logo with 85

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Logo() {
   return (
     <a href="/" className="flex items-center gap-2">
-      <img src="/Kush_Dept_Logo_Icon_White.svg" alt="Logo" className="h-12 w-16
+      <img src="/project_85_logo-512x512.svg" alt="Logo" className="h-12 w-16
       " />
       <p className="text-3xl font-bold leading-tight tracking-tighter text-blue-600">
         
