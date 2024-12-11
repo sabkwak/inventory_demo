191e49408aa6957e9ba99fa16937134c8154d82c
sabkwak <sabkwak@gmail.com>
2024-12-10 23:23:03 -0700

    Update page.tsx

--- app/(dashboard)/inventory/page.tsx
+++ app/(dashboard)/inventory/page.tsx
@@ -8,8 +8,18 @@ import { toast } from "sonner";
 import InventoryTable from "./_components/InventoryTable";
 import { Button } from "@/components/ui/button"; // Import the Button component
 import CreateProductDialog from "@/app/(dashboard)/_components/CreateProductDialog"; // Import the CreateProductDialog component
+import { useQuery } from "@tanstack/react-query";
 
-function InventoryPage() {
+
+interface Props {
+  userSettings: any;
+}
+function InventoryPage({userSettings}: Props) {
+  const userQuery = useQuery({
+    queryKey: ["products"],
+    queryFn: () => fetch(`/api/products/user`).then((res) => res.json()),
+  });
+  const user = userQuery.data;
   const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
     from: new Date(new Date().getFullYear(), 0, 1), // Set 'from' date to January 1st of this year
     to: addDays(new Date(), 1), // Set the 'to' date to tomorrow
@@ -39,7 +49,7 @@ function InventoryPage() {
               setDateRange({ from, to });
             }}
           />
-          <CreateProductDialog
+          <CreateProductDialog userSettings={user}
             trigger={<Button>Create New Ingredient</Button>}
             successCallback={() => {
               // Logic to handle after a product is successfully created
